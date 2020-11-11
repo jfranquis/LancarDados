7e011f5a18bba2d14dcc028a55b863236a425917
jfranquis <janainafranquis@hotmail.com>
2020-11-11 18:51:50 -0300

    primeira mvp lançar dados

---
+++ app/src/main/java/com/wcc/lacedados/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.wcc.lacedados
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val botaoLancarDados = findViewById<Button>(R.id.lanceDadosBotao)
+        val valorDado1: TextView = findViewById<TextView>(R.id.dado1)
+        val valorDado2: TextView = findViewById<TextView>(R.id.dado2)
+
+        botaoLancarDados.setOnClickListener{
+            valorDado1.text = lancarDados().toString()
+            valorDado2.text = lancarDados().toString()
+        }
+
+    }
+
+    fun lancarDados(): Int {
+        return (1..6).random()
+    }
+}
